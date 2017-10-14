acf66ad59bfff00e366333825d989b1055d1cf95
Qinqi "Bill" Wang <qinqi402@gmail.com>
2017-10-14 17:08:47 -0500

    Added map ideas

--- Assets/Scripts/Hostage.cs
+++ Assets/Scripts/Hostage.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 //player can pick up a hostage
 public class Hostage : MonoBehaviour {
 
+	bool hostageGet = true;
+
 	void OnTriggerEnter2D (Collider2D other) {
-		
-		if (other.gameObject.name == "Player") {
+		if (other.gameObject.name == "Player" && hostageGet == true) {
 			print ("hostage rescued!");
-			//make gameobject disappear once the player collides with is
+			//make gameobject disappear once the player collides with it
 			this.gameObject.GetComponent<Renderer> ().enabled = false;
+			hostageGet = false;
 		}
 
 	}
