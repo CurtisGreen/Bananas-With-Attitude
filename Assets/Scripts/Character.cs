29317b38f1c13d03a2fcf0f4c9f6cb091d01b6c0
Qinqi "Bill" Wang <qinqi402@gmail.com>
2017-10-13 22:37:03 -0500

    Banana animations

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	protected SpriteRenderer spriteRenderer;
+
+	public Sprite up;
+	public Sprite left;
+	public Sprite right;
+	public Sprite down;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : Character {
 
 	public Rigidbody rigidBody;
 	float moveX;
 	float moveY;
+	float velX;
+	float velY;
 	public float playerSpeed = 10;
+	Animator animator;
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +20,21 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		PlayerMove ();
+		PlayerAnim ();
 	}
 
 	void PlayerMove() {
 		this.moveX = Input.GetAxis ("Horizontal");
 		this.moveY = Input.GetAxis ("Vertical");
-
 		Vector2 movement = new Vector2 (moveX, moveY);
-		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-		rigidBody.velocity = movement * playerSpeed;	
+		rigidBody.velocity = movement * playerSpeed;
+		velX = rigidBody.velocity.x;
+		velY = rigidBody.velocity.y;
+	}
+
+	void PlayerAnim() {
+		animator = GetComponent<Animator> ();
+		animator.SetFloat ("VelX", velX);
+		animator.SetFloat ("VelY", velY);
 	}
 }
