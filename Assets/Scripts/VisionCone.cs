4b0aee62d02d1b02448387b4423ad6d148161b6d
Green000 <c.green5201@gmail.com>
2017-10-14 13:51:46 -0500

    Updated vision cones

--- Assets/Scripts/VisionCone.cs
+++ Assets/Scripts/VisionCone.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class VisionCone : MonoBehaviour {
 
-    public Transform sightStart, sightEnd;
+    public Transform sightStart, sightEnd1, sightEnd2;
 
     public bool spotted = false;
-	
-	// Update is called once per frame
-	void Update () {
+    private float playerSpeed;
+    Guard playerScript;
+
+    private void Start()
+    {
+        playerScript = this.GetComponent<Guard>();
+        playerSpeed = playerScript.playerSpeed;
+    }
+    // Update is called once per frame
+    void Update () {
         Raycasting();
         Behaviors();
-	}
+        playerSpeed = playerScript.playerSpeed;
+    }
     void Raycasting()
     {
-        Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
-        spotted = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+        if (playerSpeed > 0)
+        {
+            Debug.DrawLine(sightStart.position, sightEnd1.position, Color.green);
+            spotted = Physics2D.Linecast(sightStart.position, sightEnd1.position, 1 << LayerMask.NameToLayer("Player"));
+        }
+        else
+        {
+            Debug.DrawLine(sightStart.position, sightEnd2.position, Color.green);
+            spotted = Physics2D.Linecast(sightStart.position, sightEnd2.position, 1 << LayerMask.NameToLayer("Player"));
+        }
     }
     
     void Behaviors()
     {
-        if (spotted)
-        {
+        if (spotted){
             print("sees player");
         }
+
         
     }
 }
