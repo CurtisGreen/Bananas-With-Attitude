b3d9573402853dffcbdf05dd6e2f1d352d06371b
jingyulu94 <jingyu_lu@tamu.edu>
2017-10-15 14:54:10 -0500

    door collision

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    public Player player;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("Player").GetComponent<Player> ();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" && player.hostageCount == 4)
+        {
+            SceneManager.LoadScene("Victory Screen");
+        }
+
+    }
+}
