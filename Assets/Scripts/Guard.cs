341add7a141db33d15700c44147535c4e6d628f8
Cameron Green <cgree78@gmail.com>
2017-10-14 19:58:17 -0500

    Gorilla turns around now!

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -2,58 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Guard : Character {
+public class Guard : Character
+{
 
-	public Rigidbody rigidBody;
-	float moveX;
-	float moveY;
-	float velX;
-	float velY;
-	public float playerSpeed = 1;
-	Animator animator;
+    public Rigidbody rigidBody;
+    float moveX;
+    float moveY;
+    float velX;
+    float velY;
+    public float playerSpeed = 1;
+    Animator animator;
     public Transform currentWaypoint, point1, point2;
     //public GameObject targetA, targetB;
     public float currentPosition = 0.0f;
 
-	// Use this for initialization
-	void Start () {
+    protected BoxCollider2D boxcollider;
+
+
+    // Use this for initialization
+    void Start()
+    {
         currentWaypoint = point2;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+        {
+            spriteRenderer.sprite = up;
+        }
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //PlayerMove ();
         //PlayerAnim ();
         PaceBackForth();
-	}
+    }
     /*
      * Move the player via horiazontal and vertical inputs. update the velocity from these inputs
          */
-	void PlayerMove() {
-		this.moveX = Input.GetAxis ("Horizontal");
-		this.moveY = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveX, moveY);
-		rigidBody.velocity = movement * playerSpeed;
-		velX = rigidBody.velocity.x;
-		velY = rigidBody.velocity.y;
-	}
+    void PlayerMove()
+    {
+        this.moveX = Input.GetAxis("Horizontal");
+        this.moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY);
+        rigidBody.velocity = movement * playerSpeed;
+        velX = rigidBody.velocity.x;
+        velY = rigidBody.velocity.y;
+    }
     /*
      * animate a player's movement. Set velocity by X and Y
          */
-	void PlayerAnim() {
-		animator = GetComponent<Animator> ();
-		animator.SetFloat ("VelX", velX);
-		animator.SetFloat ("VelY", velY);
-	}
+    void PlayerAnim()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("VelX", velX);
+        animator.SetFloat("VelY", velY);
+    }
 
     /*
      * called on update:
-     * 
+     *
      * paces back and forth from current position to currentWaypoint
          */
     void PaceBackForth()
     {
-        
+
         var moveTo = currentWaypoint.transform.position.y - transform.position.y;
         if (Mathf.Abs(moveTo) <= 0.1f)
         {
@@ -63,10 +77,14 @@ public class Guard : Character {
             if (currentWaypoint == point2)
             {
                 currentWaypoint = point1;
+                Debug.Log("down");
+                this.spriteRenderer.sprite = down;
             }
             else
             {
                 currentWaypoint = point2;
+                Debug.Log("up");
+                this.spriteRenderer.sprite = up;
             }
         }
         else
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,67 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Character {
+public class Player : Character
+{
 
-	public Rigidbody2D rigidBody;
-	float moveX;
-	float moveY;
-	float velX;
-	float velY;
-	public float playerSpeed = 4;
-	Animator animator;
-	protected BoxCollider2D boxcollider;
-	public int lives = 3;
+    public Rigidbody2D rigidBody;
+    float moveX;
+    float moveY;
+    float velX;
+    float velY;
+    public float playerSpeed = 4;
+    Animator animator;
+    protected BoxCollider2D boxcollider;
+    public int lives = 3;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerSpeed = 4;
-        spriteRenderer = GetComponent<SpriteRenderer> ();
-		boxcollider = GetComponent<BoxCollider2D> ();
-		if (spriteRenderer.sprite == null) {
-			spriteRenderer.sprite = right;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		PlayerMove ();
-		PlayerAnim ();
-	}
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxcollider = GetComponent<BoxCollider2D>();
+        if (spriteRenderer.sprite == null)
+        {
+            spriteRenderer.sprite = right;
+        }
+    }
 
-	void PlayerMove() {
-		this.moveX = Input.GetAxis ("Horizontal");
-		this.moveY = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveX, moveY);
-		rigidBody.velocity = movement * playerSpeed;
-		velX = rigidBody.velocity.x;
-		velY = rigidBody.velocity.y;
-		checkDirection ();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerAnim();
+    }
 
-	void checkDirection() {
-		if(velX > 0.0f) {
-			this.spriteRenderer.sprite = right;
-			//hardcoding things is totally safe!
-			this.boxcollider.size = new Vector2(0.62f,0.57f);
-		}
-		if(velX < 0.0f) {
-			this.spriteRenderer.sprite = left;
-			this.boxcollider.size = new Vector2(0.62f,0.57f);
-		}
-		if(velY > 0.0f) {
-			this.spriteRenderer.sprite = up;
-			this.boxcollider.size = new Vector2(0.22f,0.57f);
-		}
-		if(velY < 0.0f) {
-			this.spriteRenderer.sprite = down;
-			this.boxcollider.size = new Vector2(0.22f,0.57f);
-		}
-	}
+    void PlayerMove()
+    {
+        this.moveX = Input.GetAxis("Horizontal");
+        this.moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY);
+        rigidBody.velocity = movement * playerSpeed;
+        velX = rigidBody.velocity.x;
+        velY = rigidBody.velocity.y;
+        checkDirection();
+    }
 
-	void PlayerAnim() {
-		animator = GetComponent<Animator> ();
-		animator.SetFloat ("VelX", velX);
-		animator.SetFloat ("VelY", velY);
-	}
+    void checkDirection(){
+        if (velX > 0.0f)
+        {
+            this.spriteRenderer.sprite = right;
+            //hardcoding things is totally safe!
+            this.boxcollider.size = new Vector2(0.62f, 0.57f);
+        }
+        if (velX < 0.0f)
+        {
+            this.spriteRenderer.sprite = left;
+            this.boxcollider.size = new Vector2(0.62f, 0.57f);
+        }
+        if (velY > 0.0f)
+        {
+            this.spriteRenderer.sprite = up;
+            this.boxcollider.size = new Vector2(0.22f, 0.57f);
+        }
+        if (velY < 0.0f)
+        {
+            this.spriteRenderer.sprite = down;
+            this.boxcollider.size = new Vector2(0.22f, 0.57f);
+        }
+    }
+
+    void PlayerAnim()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("VelX", velX);
+        animator.SetFloat("VelY", velY);
+    }
 }
